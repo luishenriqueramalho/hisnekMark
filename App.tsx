1589930018d68602aa1d8b50a6be3f053252d76a
luishenriqueramalho <luishenrique.ramalhodasilva@gmail.com>
2024-05-31 08:31:41 -0300

    ajustes

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { ApolloProvider } from "@apollo/client";
 import client from "graphql/client";
 import { StoreProvider } from "@/mobx/store";
 import { Amplify } from "aws-amplify";
-import amplifyconfig from "./src/aws-exports";
+import amplifyconfig from "./src/amplifyconfiguration.json";
 import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react-native";
 
 Amplify.configure(amplifyconfig);
--- graphql/client.ts
+++ graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://192.168.100.144:4000", // Substitua pelo URL do seu servidor GraphQL, se diferente
+  uri: "http://192.168.100.194:4000",
   cache: new InMemoryCache(),
 });
 
